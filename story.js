df167e0cf5daf657190a6e07c1ed0b4b572b3e29
Scott Enslin <enslin21@yahoo.com>
2017-10-17 08:36:52 -0400

    story time

---
+++ story.js
@@ -0,0 +1,5 @@
+var adjective = prompt("Please type a adjective");
+var noun = prompt("Please type a noun");
+var verb = prompt("Please type a verb");
+
+document.write("There once was a "+ adjective  + noun + " who could " + verb + " like a pro." )
